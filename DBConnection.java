429fb71e16060a8df05e4e51786b21a51c437297
Karthikeya914 <karthikeya.chakravaram@gmail.com>
2025-07-27 23:47:53 +0530

    Add files via upload

---
+++ DBConnection.java
@@ -0,0 +1,16 @@
+
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/courier_db";
+    private static final String USER = "karthikeya";
+    private static final String PASSWORD = "mypassword123";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
